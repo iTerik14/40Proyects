47bd4dddc7f67390a53f744ea93c66227c9d5a41
iTerik14 <103534577+iTerik14@users.noreply.github.com>
2023-01-11 17:44:14 -0400

    scroll2

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -1,7 +1,11 @@
 
+window.addEventListener('DOMContentLoaded',()=>{
+    let nav = document.querySelector('.nav');
+    nav.classList.add('nav_n')
+})
 
 window.addEventListener('scroll', function () {
     let nav = document.querySelector('.nav');
     nav.classList.toggle('nav_fix', window.scrollY>0);
 
-})
+});
